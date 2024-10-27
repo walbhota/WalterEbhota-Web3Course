b6433d20e53f912546cee3ae335368a2dd41f401
Walbhota <walbhota@gmail.com>
2024-10-27 20:48:00 +0100

    Added Everything excercise for module2

--- Module2/Everything.js
+++ Module2/Everything.js
@@ -1,1 +1,22 @@
-10
+function every(array, test) {
+    for (let element of array) {
+      if (!test(element)) return false;
+    }
+    return true;
+  }
+  
+  console.log(every([1, 3, 5], n => n < 10));
+  
+  console.log(every([2, 4, 16], n => n < 10));
+  
+
+  
+  function every(array, test) {
+    return !array.some(element => !test(element));
+  }
+  
+  console.log(every([1, 3, 5], n => n < 10));
+  
+  console.log(every([2, 4, 16], n => n < 10));
+  
+  
